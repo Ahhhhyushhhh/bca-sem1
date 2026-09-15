d2b27a0c8bd45a919105db5593a06898c51bde2a
Ayush Kaushik <ayushbca088@gmail.com>
2026-09-15 22:35:38 +0530

    Add ahhh.c

---
+++ ahhh.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+   
+    for(int i=10; i>=1; i=i-2){
+        printf("With Great Power Comes Great Responsibility\n");
+        printf("%d\n", i);
+    }
+    
+
+        int i=1;{
+         printf("%d\n", i++);//use then inc
+         printf("%d\n", i);
+    }
+         printf("%d\n", ++i);//inc then use
+         printf("%d\n", i);
+
+         for(float i=1.0; i<=5.0; i++){
+             printf("%f\n", i);
+         }
+
+         for(char ch='a'; ch<='z'; ch++){
+            printf("%c\n", ch);
+         }
+        }
